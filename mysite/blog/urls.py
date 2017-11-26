2ef2ad3cd345329e99f32cabfe67f2d58d4c1519
binhnguyennus <binhnguyenvnuhcm@gmail.com>
2017-11-26 10:18:12 +0800

    Create feeds for blog posts

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     # post views
@@ -11,4 +11,5 @@ urlpatterns = [
         views.post_detail,
         name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+	url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
